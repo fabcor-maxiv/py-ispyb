4f7c65e976aae02fd30131473dee4bc6953c8158
fabcor <fabien.coronis@maxiv.lu.se>
2023-11-16 17:14:48 +0100

    Fix AD authentication plugin

--- pyispyb/app/extensions/auth/ADAuthentication.py
+++ pyispyb/app/extensions/auth/ADAuthentication.py
@@ -23,7 +23,7 @@ import ldap
 
 from ispyb import models
 
-from pyispyb.app.extensions.auth.AbstractAuthentication import AbstractAuthentication
+from .AbstractAuthentication import AbstractAuthentication
 
 
 __license__ = "LGPLv3+"
@@ -33,14 +33,14 @@ log = logging.getLogger(__name__)
 
 
 class ADAuthentication(AbstractAuthentication):
-    def __init__(self):
-        AbstractAuthentication.__init__(self)
 
-        self.ad_conn = None
-        self.ad_base_auth = None
-        self.ad_base_internal = None
-        self.ad_uri = None
-        self.ad_domain = None
+    def __init__(self):
+        super().__init__(self)  # TODO not entirely sure we need this since there is no __init__
+
+        # Configuration settings
+        self.ad_base_dn = ""  # base DN
+        self.ad_domain = ""
+        self.ad_uri = ""
 
     def configure(self, config: dict[str, Any]):
         """Configure auth plugin.
@@ -48,101 +48,123 @@ class ADAuthentication(AbstractAuthentication):
         Args:
             config (dict): plugin configuration from file
         """
+        self.ad_base_dn = config["AD_BASE_DN"]
+        self.ad_domain = config["AD_DOMAIN"]
         self.ad_uri = config["AD_URI"]
-        self.ad_base_auth = config["AD_BASE_AUTH"]
-        self.ad_base_internal = "cn=Users,dc=maxlab,dc=lu,dc=se"
-        self.ad_domain = config["AD_USERPRINCIPALNAMEDOMAIN"]
 
-    def authenticate_by_login(
-        self, login: str, password: str
-    ) -> Optional[models.Person]:
-        groups = None
-        group_list = None
+    def authenticate_by_login(self, login: str, password: str) -> Optional[models.Person]:
+
+        log.debug("AD login: try to authenticate user `%s`" % login)
+        login = login.strip()
+
+        if not login:
+            log.debug("AD login: can not authenticate without login")
+            return None
+
+        who = f"{login}@{self.ad_domain}"
+        log.debug("AD login: try to authenticate user `%s`" % who)
+
         try:
-            log.debug(
-                f"AD login: try to authenticate user `{login}` "
-            )
-            self.ad_conn = ldap.initialize(self.ad_uri)
-            search_str = login + self.ad_domain 
-            self.ad_conn.protocol_version = ldap.VERSION3
-            self.ad_conn.set_option(ldap.OPT_REFERRALS, 0)
-            self.ad_conn.simple_bind_s(search_str, password)
-            log.debug("user authenticated... waiting to have all the attributes")
-            print("user authenticated... waiting to have all the attributes")
-            # self.ad_conn.simple_bind_s(
-            #     f"cn={login},{self.ad_base_auth}", password
-            # )
-            res = self.ad_conn.search_s(
-                self.ad_base_auth,
+            ad_conn = ldap.initialize(self.ad_uri)
+        except Exception as exc:  # TODO Check actual exception type
+            log.exception("AD login: can not initialize connection")
+            return None
+
+        ad_conn.protocol_version = ldap.VERSION3
+        ad_conn.set_option(ldap.OPT_REFERRALS, 0)
+
+        try:
+            ad_conn.simple_bind_s(who, password)
+        except ldap.INVALID_CREDENTIALS:
+            log.exception("AD login: unable to authenticate user `%s`" % login)
+            return None
+
+        log.debug("AD login: user authenticated... waiting to have all the attributes")
+
+        try:
+            res = ad_conn.search_s(
+                self.ad_base_dn,
                 ldap.SCOPE_SUBTREE,
                 f"(SAMAccountname={login})", 
-                ['mail', 'uidNumber', 'sn', 'givenName', 'telephoneNumber', 'memberOf'])[0][1]
-            log.debug(res)
-            print(res)
-
-            def get_value(v: str):
-                if v in res:
-                    return res[v][0]
-                return None
-            
-            group_list = res["memberOf"]
-            for group in group_list:
-                print(group)
-                groups += [group]
-
-            return models.Person(
-                login=login,
-                emailAddress=get_value("mail"),
-                siteId=get_value("uidNumber"),
-                familyName=get_value("sn"),
-                givenName=get_value("givenName"),
-                phoneNumber=get_value("telephoneNumber"),
-            )
+                ['mail', 'uidNumber', 'sn', 'givenName', 'telephoneNumber', 'memberOf'],
+            )[0][1]
         except ldap.INVALID_CREDENTIALS:
-            log.exception(f"AD login: unable to authenticate user {login}")
+            log.exception("AD login: unable to authenticate user `%s`" % login)
+            return None
 
+        log.debug("AD login: search result: %s" % res)
+
+        def get_value(v: str):
+            if v in res:
+                return res[v][0]
+            return None
+        
+        return models.Person(
+            login=login,
+            emailAddress=get_value("mail"),
+            siteId=get_value("uidNumber"),
+            familyName=get_value("sn"),
+            givenName=get_value("givenName"),
+            phoneNumber=get_value("telephoneNumber"),
+        )
+
+
+    # TODO Do we need this?
     def get_user_and_groups(
-        self, username: str | None, password: str | None, token: str | None
+        self, username: str | None, password: str | None, _token: str | None
     ) -> tuple[str | None, list[str] | None]:
         """Return username and groups associated to the user.
 
         Args:
             username (string): auth username
             password (string): auth password
-            token (string): auth token
+            _token (string): auth token (unused)
         Returns:
             username, groups
         """
-        user = None
+
+        username = username.strip()
+
+        msg = "AD login: try to authenticate user `%s`" % username
+        log.debug(msg)
+
+        if not username:
+            log.debug("AD login: can not authenticate without username")
+            return None, None
+
         groups = None
         group_list = None
+        who = f"{username}@{self.ad_domain}"
+        search_filter = f"(samAccountName={username})"
+        attrs = ["*"]
+
+        msg = "AD login: try to authenticate user `%s`" % who
+        log.debug(msg)
+
         try:
-            msg = "AD login: try to authenticate user %s " % username
-            log.debug(msg)
-            search_filter = "(samAccountName=%s)" % username
-            attrs = ["*"]
-            #search_str = self.ldap_id + "=" + username + "," + self.ldap_base_internal
-            search_str = username + self.ad_domain 
-            self.ad_conn.simple_bind_s(search_str, password)
-            result = self.ad_conn.search_s(
-                self.ad_base_auth,
+            ad_conn = ldap.initialize(self.ad_uri)
+        except Exception as exc:  # TODO Check actual exception type
+            log.exception("AD login: can not initialize connection")
+            return None, None
+
+        try:
+            ad_conn.simple_bind_s(search_str, password)
+            result = ad_conn.search_s(
+                self.ad_base_dn,
                 ldap.SCOPE_SUBTREE,
                 search_filter,
                 attrs,
             )
-            #print(result)
-            user = username
-            group_list = result[0][1]["memberOf"]
-            for group in group_list:
-                groups.append(group)
         except ldap.INVALID_CREDENTIALS as ex:
-            msg = "AD login: unable to authenticate user %s (%s)" % (
-                username,
-                str(ex),
-            )
+            msg = "AD login: unable to authenticate user `%s`" % who
             log.exception(msg)
             return None, None
         
+        group_list = result[0][1]["memberOf"]
+        for group in group_list:
+            groups.append(group)
+
         if user is None or groups is None:
-            return None, None, None
+            return None, None
+
         return user, groups
